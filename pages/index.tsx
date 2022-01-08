27f6166180439126d228dfd1c859ca8cbcf96fc2
Alexandre Chabrolin <9203826+chabroA@users.noreply.github.com>
2022-01-08 10:14:50 +0100

    wip: add sell page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,46 @@
 import React, { useState } from "react";
 
+import { ExchangeType } from "@ledgerhq/live-app-sdk";
+
 import Swap from "./swap";
 import Fund from "./fund";
+import Sell from "./sell";
 
 import styles from "../styles/Home.module.css";
 
+const ExchangeFlow = ({ exchangeType }: { exchangeType: ExchangeType }) => {
+  switch (exchangeType) {
+    case ExchangeType.SWAP:
+      return <Swap />;
+
+    case ExchangeType.FUND:
+      return <Fund />;
+
+    case ExchangeType.SELL:
+      return <Sell />;
+
+    default:
+      return <div />;
+  }
+};
+
 const Home = () => {
-  const [mode, setMode] = useState("swap");
+  const [exchangeType, setExchangeType] = useState(ExchangeType.SWAP);
 
   return (
     <div>
       <div className={styles.header}>
-        <button onClick={() => setMode("swap")}>{"Swap flow"}</button>
-        <button onClick={() => setMode("fund")}>{"Fund flow"}</button>
+        <button onClick={() => setExchangeType(ExchangeType.SWAP)}>
+          {"Swap flow"}
+        </button>
+        <button onClick={() => setExchangeType(ExchangeType.FUND)}>
+          {"Fund flow"}
+        </button>
+        <button onClick={() => setExchangeType(ExchangeType.SELL)}>
+          {"Sell flow"}
+        </button>
       </div>
-      {mode === "swap" ? <Swap /> : <Fund />}
+      <ExchangeFlow exchangeType={exchangeType} />
     </div>
   );
 };
